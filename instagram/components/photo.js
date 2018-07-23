dc84a3bde727ac4840307e908c7aaee2e370e925
DeegiiAlt <aagiideegii@gmail.com>
2018-07-23 17:26:21 -0600

    another comment

--- instagram/components/photo.js
+++ instagram/components/photo.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Image, TouchableOpacity, Camera } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 // example
+// example
 
 class Photo extends Component {
   takePicture() {
